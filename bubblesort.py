df15f20b444370d843ac5dd00f8b2ef0c86dae95
Taral <tvpatel01@gmail.com>
2019-07-03 19:16:32 -0400

    Bubble sort algorithm modification for more efficient running

--- bubblesort.py
+++ bubblesort.py
@@ -1,14 +1,14 @@
 # bubblesort program 
 
-numbers = [2, 5, 1, 9, 0, -15, 3, 11, 4, 90, 11, 7, 12, 962, 43, 20]
+numbers = [2, 5, 1, 19, 0, -8]
 
 def bubble_sort(numbers):
     for i in range(0, len(numbers) - 1):
-        for j in range(0, len(numbers) - 1):
+        for j in range(0, len(numbers) - 1 - i):
             if numbers[j] > numbers[j+1]:
-                Q = numbers[j]
+                q = numbers[j]
                 numbers[j] = numbers[j+1]
-                numbers[j+1] = Q
+                numbers[j+1] = q
 
 bubble_sort(numbers)
 print(numbers)
